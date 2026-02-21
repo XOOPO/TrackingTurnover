f7f4e74679bfe42b8df358942c80c59574868932
XOOPO <apexnovaop008@gmail.com>
2026-02-21 20:19:02 +0800

    Add files via upload

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./drizzle/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+
+  // ⬇️ TAMBAHKAN INI (PENTING)
+  migrationsSchema: "public",
+  migrationsTable: "__drizzle_migrations",
+});
